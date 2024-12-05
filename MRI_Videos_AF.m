6b2792de3e5f8bdf15357f598e7925541ec987e7
AndrewFischer <andrew_debbie@yahoo.com>
2024-12-05 10:18:41 +0000

    Remove unimportant comment

--- MRI_Videos_AF.m
+++ MRI_Videos_AF.m
@@ -2,7 +2,6 @@
 % 
 %% Development / Production Settings
 % Setting devmode to 1 speeds things up, enables keyboard...
-% unimportant. 
 % For production, set devMode = 0.
 devMode = 1;
 if (devMode == 1)
